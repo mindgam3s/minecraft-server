08b307775f4aa37f66ab79ecc43f5deb430dfcf3
mindgam3s <34487011+mindgam3s@users.noreply.github.com>
2025-11-22 12:53:42 +0100

    Add files via upload

---
+++ stop.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+/home/minecraft/mcserver/mcrcon/mcrcon -H localhost -P 25575 -p 'mnVuBqPliwiQ5Ppz@>jA0LK^60dI-ZiS,xfqd}>b+YoOqSgK&7ldWiQibHjfbXjV' 'say Stopping server ...'
+/home/minecraft/mcserver/mcrcon/mcrcon -H localhost -P 25575 -p 'mnVuBqPliwiQ5Ppz@>jA0LK^60dI-ZiS,xfqd}>b+YoOqSgK&7ldWiQibHjfbXjV' 'save-off'
+/home/minecraft/mcserver/mcrcon/mcrcon -H localhost -P 25575 -p 'mnVuBqPliwiQ5Ppz@>jA0LK^60dI-ZiS,xfqd}>b+YoOqSgK&7ldWiQibHjfbXjV' 'save-all flush'
+/home/minecraft/mcserver/mcrcon/mcrcon -H localhost -P 25575 -p 'mnVuBqPliwiQ5Ppz@>jA0LK^60dI-ZiS,xfqd}>b+YoOqSgK&7ldWiQibHjfbXjV' 'save-on'
+/home/minecraft/mcserver/mcrcon/mcrcon -H localhost -P 25575 -p 'mnVuBqPliwiQ5Ppz@>jA0LK^60dI-ZiS,xfqd}>b+YoOqSgK&7ldWiQibHjfbXjV' 'stop'
